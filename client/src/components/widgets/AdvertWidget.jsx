2aa49adec4886b1449c4667cc4471b3ed8d1047c
Om-Aditya-Jain <omadityajain@gmail.com>
2023-08-31 23:04:22 +0530

    completed Home Page and Profile Page

---
+++ client/src/components/widgets/AdvertWidget.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "../FlexBetween";
+import WidgetWrapper from "../WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsered
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        src="http://localhost:3001/assets/info4.jpg"
+        alt="advertisement"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>Mika Cosmetics</Typography>
+        <Typography color={medium}>mikacosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Your pathway to stunning and immaculate beauty and made sure your skin
+        is exfoliating skin and shining like light.
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
---
+++ client/src/components/widgets/FriendListWidget.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import Friend from "../Friend";
+import WidgetWrapper from "../WidgetWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "../../state";
+
+const FriendListWidget = ({ userId }) => {
+  const dispatch = useDispatch();
+  const { palette } = useTheme();
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  const getFriends = async () => {
+    const response = await fetch(`http://localhost/users/${userId}/friends`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  return (
+    <WidgetWrapper>
+      <Typography
+        color={palette.neutral.dark}
+        variant="h5"
+        fontWeight="500"
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend List
+      </Typography>
+      <Box display="flex" flexDirection="column" gap="1.5rem">
+        {friends.map((friend) => (
+          <Friend
+            key={friend._id}
+            friendId={friend._id}
+            name={`${firstName} ${lastName}`}
+            subtitle={friend.occupation}
+            userPicturePath={friend.picturePath}
+          />
+        ))}
+      </Box>
+    </WidgetWrapper>
+  );
+};
+
+export default FriendListWidget;

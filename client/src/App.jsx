27591976984e6c67d33599d04e9620c12f3fdb64
Om-Aditya-Jain <omadityajain@gmail.com>
2023-09-02 17:49:43 +0530

    improved add friend functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,29 +9,29 @@ import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { themeSettings } from "./theme";
 
 function App() {
-	const mode = useSelector((state) => state.mode);
-	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-	const isAuth = Boolean(useSelector((state) => state.token));
-	return (
-		<div className="app">
-			<BrowserRouter>
-				<ThemeProvider theme={theme}>
-					<CssBaseline />
-					<Routes>
-						<Route path="/" element={<Login />} />
-						<Route
-							path="/home"
-							element={isAuth ? <Home /> : <Navigate to="/" />}
-						/>
-						<Route
-							path="/profile/:userId"
-							element={isAuth ? <Profile /> : <Navigate to="/" />}
-						/>
-					</Routes>
-				</ThemeProvider>
-			</BrowserRouter>
-		</div>
-	);
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/home"
+              element={isAuth ? <Home /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <Profile /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/widgets/FriendListWidget.jsx
+++ client/src/components/widgets/FriendListWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import Friend from "../Friend";
 import WidgetWrapper from "../WidgetWrapper";
@@ -6,50 +6,55 @@ import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "../../state";
 
 const FriendListWidget = ({ userId }) => {
-	const dispatch = useDispatch();
-	const { palette } = useTheme();
-	const token = useSelector((state) => state.token);
-	const friends = useSelector((state) => state.user.friends);
+  const [friends, setFriendsData] = useState([]);
+  const dispatch = useDispatch();
+  const { palette } = useTheme();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  // const friends = useSelector((state) => state.user.friends);
 
-	const getFriends = async () => {
-		const response = await fetch(
-			`http://localhost:3001/users/${userId}/friends`,
-			{
-				method: "GET",
-				headers: { Authorization: `Bearer ${token}` },
-			}
-		);
-		const data = await response.json();
-		dispatch(setFriends({ friends: data }));
-	};
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    setFriendsData(data);
+    if (userId === loggedInUserId) {
+      dispatch(setFriends({ friends: data }));
+    }
+  };
 
-	useEffect(() => {
-		getFriends();
-	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    getFriends();
+  }, [friends]); // eslint-disable-line react-hooks/exhaustive-deps
 
-	return (
-		<WidgetWrapper>
-			<Typography
-				color={palette.neutral.dark}
-				variant="h5"
-				fontWeight="500"
-				sx={{ mb: "1.5rem" }}
-			>
-				Friend List
-			</Typography>
-			<Box display="flex" flexDirection="column" gap="1.5rem">
-				{friends.map((friend) => (
-					<Friend
-						key={friend._id}
-						friendId={friend._id}
-						name={`${friend.firstName} ${friend.lastName}`}
-						subtitle={friend.occupation}
-						userPicturePath={friend.picturePath}
-					/>
-				))}
-			</Box>
-		</WidgetWrapper>
-	);
+  return (
+    <WidgetWrapper>
+      <Typography
+        color={palette.neutral.dark}
+        variant="h5"
+        fontWeight="500"
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend List
+      </Typography>
+      <Box display="flex" flexDirection="column" gap="1.5rem">
+        {friends?.map((friend) => (
+          <Friend
+            key={friend._id}
+            friendId={friend._id}
+            name={`${friend.firstName} ${friend.lastName}`}
+            subtitle={friend.occupation}
+            userPicturePath={friend.picturePath}
+          />
+        ))}
+      </Box>
+    </WidgetWrapper>
+  );
 };
 
 export default FriendListWidget;

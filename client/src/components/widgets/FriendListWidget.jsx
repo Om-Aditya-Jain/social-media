b877aa2aa78b251c47c4bd70c1a8d0c287af9818
Om-Aditya-Jain <0808io201033.ies@ipsacademy.org>
2023-09-04 11:10:22 +0530

    updated friend list

--- client/src/components/widgets/FriendListWidget.jsx
+++ client/src/components/widgets/FriendListWidget.jsx
@@ -6,55 +6,67 @@ import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "../../state";
 
 const FriendListWidget = ({ userId }) => {
-  const [friends, setFriendsData] = useState([]);
-  const dispatch = useDispatch();
-  const { palette } = useTheme();
-  const token = useSelector((state) => state.token);
-  const loggedInUserId = useSelector((state) => state.user._id);
+	const [friendsData, setFriendsData] = useState([]);
+	const dispatch = useDispatch();
+	const { palette } = useTheme();
+	const token = useSelector((state) => state.token);
+	const friends = useSelector((state) => state.user.friends);
+	const loggedInUserId = useSelector((state) => state.user._id);
 
-  const getFriends = async () => {
-    const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
-      {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
-    const data = await response.json();
-    setFriendsData(data);
-    if (userId === loggedInUserId) {
-      dispatch(setFriends({ friends: data }));
-    }
-  };
+	const getFriends = async () => {
+		const response = await fetch(
+			`http://localhost:3001/users/${userId}/friends`,
+			{
+				method: "GET",
+				headers: { Authorization: `Bearer ${token}` },
+			}
+		);
+		const data = await response.json();
+		setFriendsData(data);
+		if (userId === loggedInUserId) {
+			dispatch(setFriends({ friends: data }));
+		}
+	};
 
-  useEffect(() => {
-    getFriends();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+	useEffect(() => {
+		getFriends();
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
-    <WidgetWrapper>
-      <Typography
-        color={palette.neutral.dark}
-        variant="h5"
-        fontWeight="500"
-        sx={{ mb: "1.5rem" }}
-      >
-        Friend List
-      </Typography>
-      <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends?.map((friend) => (
-          <Friend
-            key={friend._id}
-            friendId={friend._id}
-            name={`${friend.firstName} ${friend.lastName}`}
-            subtitle={friend.occupation}
-            userPicturePath={friend.picturePath}
-            postId={0}
-          />
-        ))}
-      </Box>
-    </WidgetWrapper>
-  );
+	return (
+		<WidgetWrapper>
+			<Typography
+				color={palette.neutral.dark}
+				variant="h5"
+				fontWeight="500"
+				sx={{ mb: "1.5rem" }}
+			>
+				Friend List
+			</Typography>
+			<Box display="flex" flexDirection="column" gap="1.5rem">
+				{userId === loggedInUserId
+					? friends?.map((friend) => (
+							<Friend
+								key={friend._id}
+								friendId={friend._id}
+								name={`${friend.firstName} ${friend.lastName}`}
+								subtitle={friend.occupation}
+								userPicturePath={friend.picturePath}
+								postId={0}
+							/>
+					  ))
+					: friendsData?.map((friend) => (
+							<Friend
+								key={friend._id}
+								friendId={friend._id}
+								name={`${friend.firstName} ${friend.lastName}`}
+								subtitle={friend.occupation}
+								userPicturePath={friend.picturePath}
+								postId={0}
+							/>
+					  ))}
+			</Box>
+		</WidgetWrapper>
+	);
 };
 
 export default FriendListWidget;
